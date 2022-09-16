de3b9a72066383a01a18bfd32f2b9ff6abef8b7e
Setsuke <sergio_alva99@hotmail.com>
2022-09-16 05:00:42 -0500

    modificacion del generador para evitar repeticion en contextos iniciales generados

--- contexto/views.py
+++ contexto/views.py
@@ -2,16 +2,15 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 from contexto.models import Contexto
 from contexto.serializers import ContextoSerializer
-import time
 from rest_framework.decorators import action
 from happytransformer import GENSettings
 from happytransformer import HappyGeneration
 from asgiref.sync import sync_to_async
+import time
 from googletrans import Translator
 
-happy_gen = HappyGeneration("GPT-NEO-1.3B", "EleutherAI/gpt-neo-1.3B")
-args = GENSettings(no_repeat_ngram_size=2)
-
+happy_gen = HappyGeneration("GPT-NEO", "EleutherAI/gpt-neo-1.3B")
+argsIA = GENSettings(no_repeat_ngram_size=2,temperature=0.7,min_length=20,top_k=50, do_sample=True)
 # Create your views here.
 class ContextoViewSet(viewsets.ModelViewSet):
     """
@@ -24,13 +23,13 @@ class ContextoViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['POST'], name='create context')
     def create(self, request,*args, **kwargs):
         context_data = request.data
-        result = happy_gen.generate_text(context_data["text_generated"])
+        result = happy_gen.generate_text(context_data["text_generated"],args=argsIA)
         translator = Translator()
 
         translated_text = translator.translate(result.text, dest='es')
         print(translated_text.text)
 
-        time.sleep(1)   
+        time.sleep(5)   
         new_context = Contexto.objects.create(text_generated = translated_text.text )
         new_context.save()
         serializer = ContextoSerializer(new_context)
@@ -48,7 +47,7 @@ class ContextoOriginalViewSet(viewsets.ModelViewSet):
     @action(detail=True, methods=['POST'], name='create context inglish')
     def create(self, request,*args, **kwargs):
         context_data = request.data
-        result = happy_gen.generate_text(context_data["text_generated"])
+        result = happy_gen.generate_text(context_data["text_generated"],args=argsIA)
 
         time.sleep(5)   
         new_context = Contexto.objects.create(text_generated = result.text )
