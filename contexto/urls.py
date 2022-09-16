b7340bccb116bf2015acc20159c6356fd2639a4e
Arvin <45604781+NivraLeak@users.noreply.github.com>
2022-09-15 21:46:42 -0500

    HU-15 Agregando crud

---
+++ contexto/urls.py
@@ -0,0 +1,20 @@
+from contexto.views import ContextoViewSet
+from rest_framework.urlpatterns import format_suffix_patterns
+from django.urls import path
+
+contexto_list = ContextoViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+contexto_detail = ContextoViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+
+
+urlpatterns = format_suffix_patterns([
+    path('contexto/', contexto_list, name='contexto-list'),
+    path('contexto/<int:pk>/', contexto_detail, name='contexto-detail'),
+])
---
+++ contexto/views.py
@@ -0,0 +1,22 @@
+from rest_framework import viewsets
+from rest_framework.response import Response
+from contexto.models import Contexto
+from contexto.serializers import ContextoSerializer
+from rest_framework.decorators import action
+from rest_framework import renderers
+
+# Create your views here.
+class ContextoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Contexto.objects.all()
+    serializer_class = ContextoSerializer
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
