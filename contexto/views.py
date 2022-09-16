9353dc217aac59d66ed9f317e457a5c61e8eadd7
Arvin <45604781+NivraLeak@users.noreply.github.com>
2022-09-15 22:44:01 -0500

    Se agrega el modelo

--- contexto/views.py
+++ contexto/views.py
@@ -3,13 +3,11 @@ from rest_framework.response import Response
 from contexto.models import Contexto
 from contexto.serializers import ContextoSerializer
 from rest_framework.decorators import action
-from rest_framework import renderers
-from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModel
 
-tokenizer = AutoTokenizer.from_pretrained("EleutherAI/gpt-j-6B")
 
-model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-j-6B")
-
+tokenizer = AutoTokenizer.from_pretrained("KoboldAI/GPT-J-6B-Adventure")
+model = AutoModelForCausalLM.from_pretrained("KoboldAI/GPT-J-6B-Adventure")
 # Create your views here.
 class ContextoViewSet(viewsets.ModelViewSet):
     """
@@ -18,8 +16,16 @@ class ContextoViewSet(viewsets.ModelViewSet):
     queryset = Contexto.objects.all()
     serializer_class = ContextoSerializer
 
-    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
-    def highlight(self, request, *args, **kwargs):
-        print("BODY: ")
-        contexto = self.get_object()
-        return Response(contexto.highlighted)
+
+    @action(detail=True, methods=['POST'], name='create context')
+    def create(self, request,*args, **kwargs):
+        inputs = tokenizer("The god of sun came to the earth to help the inca empire", return_tensors="pt")
+        outputs = model(**inputs)
+
+
+        context_data = request.data
+        print("DATA obtenida de tu post: ", outputs)
+        new_context = Contexto.objects.create(text_generated=context_data["text_generated"] + "Holi prro" )
+        new_context.save()
+        serializer = ContextoSerializer(new_context)
+        return Response(serializer.data)
