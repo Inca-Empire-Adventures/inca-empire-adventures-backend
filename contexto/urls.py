49e66013ba7360297aaf3025bb022f9bd4a09baf
Arvin <45604781+NivraLeak@users.noreply.github.com>
2022-09-21 17:49:32 -0500

    HU-15 Nuevos cambios

--- contexto/urls.py
+++ contexto/urls.py
@@ -1,4 +1,4 @@
-from contexto.views import ContextoViewSet
+from contexto.views import ContextoOriginalViewSet, ContextoViewSet
 from rest_framework.urlpatterns import format_suffix_patterns
 from django.urls import path
 
@@ -12,9 +12,13 @@ contexto_detail = ContextoViewSet.as_view({
     'patch': 'partial_update',
     'delete': 'destroy'
 })
-
+contexto_original_list = ContextoOriginalViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
 
 urlpatterns = format_suffix_patterns([
     path('', contexto_list, name='contexto-list'),
     path('<int:pk>', contexto_detail, name='contexto-detail'),
+    path('original',contexto_original_list, name='contexto-original-detail')
 ])
--- contexto/views.py
+++ contexto/views.py
@@ -3,11 +3,14 @@ from rest_framework.response import Response
 from contexto.models import Contexto
 from contexto.serializers import ContextoSerializer
 from rest_framework.decorators import action
-from transformers import AutoTokenizer, AutoModelForCausalLM, AutoModel
+from happytransformer import GENSettings
+from happytransformer import HappyGeneration
+from asgiref.sync import sync_to_async
+import time
+from googletrans import Translator
 
-
-tokenizer = AutoTokenizer.from_pretrained("KoboldAI/GPT-J-6B-Adventure")
-model = AutoModelForCausalLM.from_pretrained("KoboldAI/GPT-J-6B-Adventure")
+happy_gen = HappyGeneration("GPT-NEO", "EleutherAI/gpt-neo-1.3B")
+argsIA = GENSettings(no_repeat_ngram_size=2,temperature=0.7,min_length=20,top_k=50, do_sample=True)
 # Create your views here.
 class ContextoViewSet(viewsets.ModelViewSet):
     """
@@ -19,13 +22,35 @@ class ContextoViewSet(viewsets.ModelViewSet):
 
     @action(detail=True, methods=['POST'], name='create context')
     def create(self, request,*args, **kwargs):
-        inputs = tokenizer("The god of sun came to the earth to help the inca empire", return_tensors="pt")
-        outputs = model(**inputs)
-
-
         context_data = request.data
-        print("DATA obtenida de tu post: ", outputs)
-        new_context = Contexto.objects.create(text_generated=context_data["text_generated"] + "Holi prro" )
+        result = happy_gen.generate_text(context_data["text_generated"],args=argsIA)
+        translator = Translator()
+
+        translated_text = translator.translate(result.text, dest='es')
+        print(translated_text.text)
+
+        time.sleep(1)   
+        new_context = Contexto.objects.create(text_generated = translated_text.text )
+        new_context.save()
+        serializer = ContextoSerializer(new_context)
+        return Response(serializer.data)
+
+
+class ContextoOriginalViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Contexto.objects.all()
+    serializer_class = ContextoSerializer
+
+
+    @action(detail=True, methods=['POST'], name='create context inglish')
+    def create(self, request,*args, **kwargs):
+        context_data = request.data
+        result = happy_gen.generate_text(context_data["text_generated"],args=argsIA)
+
+        time.sleep(1)   
+        new_context = Contexto.objects.create(text_generated = result.text )
         new_context.save()
         serializer = ContextoSerializer(new_context)
         return Response(serializer.data)
