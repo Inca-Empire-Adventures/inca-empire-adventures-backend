34e55cc9cb75210d0e3b4ff15eb1026bd7adacd5
Arvin <45604781+NivraLeak@users.noreply.github.com>
2022-09-21 19:11:23 -0500

    master añadiendo cors

--- inca_empire_adventures_backend/settings.py
+++ inca_empire_adventures_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'personaje',
     'contexto',
     'races',
@@ -54,6 +55,13 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:4200',
 ]
 
 ROOT_URLCONF = 'inca_empire_adventures_backend.urls'
