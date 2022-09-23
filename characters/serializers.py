84d034bf82c8363baeabfde3a2234e222dfe0a78
Arvin <45604781+NivraLeak@users.noreply.github.com>
2022-09-23 01:52:23 -0500

    master se cambian a objetos en vez de id

--- characters/serializers.py
+++ characters/serializers.py
@@ -1,12 +1,15 @@
 from rest_framework import serializers
 from characters.models import Character
 from professions.models import Profession
+from professions.serializers import ProfessionSerializer
 from races.models import Race
+from races.serializers import RaceSerializer
 
 class CharacterSerializer(serializers.ModelSerializer):
-    profession = serializers.PrimaryKeyRelatedField(many=False, queryset=Profession.objects.all(), allow_null = True)
-    race = serializers.PrimaryKeyRelatedField(many=False, queryset=Race.objects.all(), allow_null = True)
-
+    #profession = serializers.PrimaryKeyRelatedField(many=False, queryset=Profession.objects.all(), allow_null = True)
+    #race = serializers.PrimaryKeyRelatedField(many=False, queryset=Race.objects.all(), allow_null = True)
+    profession = ProfessionSerializer(many=False,allow_null= True)
+    race = RaceSerializer(many=False,allow_null = True)
     class Meta:
         model = Character
         fields = ['id', 'nameGroup', 'namePlayer', 'profession', 'race', 'url']
