af3319e865ea62e59b4a187c66b2d7444bdef7e1
Arvin <45604781+NivraLeak@users.noreply.github.com>
2023-04-03 00:22:35 -0500

    Se agrega correccion de statistic

--- characters/serializers.py
+++ characters/serializers.py
@@ -8,8 +8,12 @@ class CharacterSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(many=False, queryset=User.objects.all(), allow_null=True)
 
     def validate_statistic(self, value):
-        if value is not None and Character.objects.filter(statistic=value).exclude(pk=self.instance.pk).exists():
+        if value is not None and self.instance is not None and Character.objects.filter(statistic=value).exclude(pk=self.instance.pk).exists():
             raise serializers.ValidationError("This statistics has already been assigned to another Character.")
+
+        if self.instance is None and Character.objects.filter(statistic = value).exists():
+            raise serializers.ValidationError("This statistics has already been assigned to another Character.")
+
         return value
     class Meta:
         model = Character
