1ec6775207e58e34210d2db9acaea20a0cee9534
Arvin <45604781+NivraLeak@users.noreply.github.com>
2023-04-27 18:54:08 -0500

    master se agregan archivos statics

--- inca_empire_adventures_backend/settings_testing.py
+++ inca_empire_adventures_backend/settings_testing.py
@@ -154,7 +154,9 @@ SECURE_SSL_REDIRECT = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_ROOT = '/inca_empire_adventures_backend'
+STATIC_URL = 'staticfiles/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
