95bd029a5f4f98fd18bc899f0fbed4dc586d44af
Arvin <agarciagodos@itana.pe>
2023-05-28 13:31:35 -0500

    master ultimos cambios

--- statistics_user/enums.py
+++ statistics_user/enums.py
@@ -3,7 +3,7 @@ from django.utils.translation import gettext_lazy as _
 
 
 class EthnicityType(models.TextChoices):
-    GOD_OF_SUN = "GOD_OF_SUN", _('Dios del Sol')
-    GOD_OF_DEATH = "GOD_OF_DEATH", _('Dios de la Muerte')
-    GOD_OF_MOON = "GOD_OF_MOON", _('Dios de la Luna')
-    GOD_OF_EARTH = "GOD_OF_EARTH", _('Dios de la Tierra')
+    GOD_OF_SUN = "Dios del Sol", _('Dios del Sol')
+    GOD_OF_DEATH = "Dios de la Muerte", _('Dios de la Muerte')
+    GOD_OF_MOON = "Dios de la Luna", _('Dios de la Luna')
+    GOD_OF_EARTH = "Dios de la Tierra", _('Dios de la Tierra')
